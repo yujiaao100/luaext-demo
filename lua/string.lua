9c7cbc6770133fdfa0e2f56874bca2a98076b626
test <test@rayjoy.com>
2016-12-10 04:25:03 +0800

    update ver

--- lua/string.lua
+++ lua/string.lua
@@ -1,18 +1,8 @@
-test="111 222 333"
-function split(s, delim)
-    if type(delim) ~= "string" or string.len(delim) <= 0 then
-        return
-    end
-    local start = 1
-    local t = {}
-    while true do
-    local pos = string.find (s, delim, start, true) -- plain find
-        if not pos then
-          break
-        end
-        table.insert (t, string.sub (s, start, pos - 1))
-        start = pos + string.len (delim)
-    end
-    table.insert (t, string.sub (s, start))
-    return t
+string.split = function(s, p)
+    local rt= {}
+    string.gsub(s, '[^'..p..']+', function(w) table.insert(rt, w) end )
+    return rt
+end
+string.trim =function(s)
+	return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
